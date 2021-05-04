2dcf9ee595d11ffea666a6ffcb48c0774ff8d40e
Prashant Chaudhari <prashantchauhdhari881@gmail.com>
2021-05-04 08:58:07 +0530

    partly finish fibonacci number again problem

--- Algorithmic_Toolbox/code_runner/src/main.rs
+++ Algorithmic_Toolbox/code_runner/src/main.rs
@@ -1,27 +1,55 @@
+use std::convert::TryInto;
 use std::io;
-use std::process;
 
 fn main() {
-    let n: u128 = read_length();
-    println!("{}", n);
+    let nums: [u128; 2] = read_numbers();
+
+    let [_a, b] = nums;
+
+    let period_length: u128 = get_pisano_period(b);
+
+    println!("{}", period_length);
 }
 
-fn read_length() -> u128 {
-    let mut input = String::new();
+fn get_pisano_period(m: u128) -> u128 {
+    let mut curr: u32 = 0;
+    let mut next: u32 = 1;
+    let mut r: u32 = curr + next;
+    let mut count: u32 = 0;
+    let max_limit: u32 = m.try_into().unwrap();
+    let mut result = 2;
 
-    io::stdin().read_line(&mut input).unwrap();
+    // based on thoery that period of Fi mod m does not exceed m^2
+    while count < max_limit * max_limit {
+        r = (curr + next) % max_limit;
+        curr = next;
+        next = r;
 
-    let digit: u128;
-
-    match input.trim().parse() {
-        Ok(val) => {
-            digit = val;
+        if curr == 0 && next == 1 {
+            result = (count + 1).try_into().unwrap()
         }
-        Err(_err) => {
-            println!("Please enter valid number.");
-            process::exit(1);
-        }
+
+        count += 1
     }
 
-    digit
+    result
+}
+
+fn get_fibonacci_huge(n: u128, m: u128) {
+    // TODO: finish this function
+}
+
+fn read_numbers() -> [u128; 2] {
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("User input failed");
+    let mut words = input.split_whitespace();
+
+    // convert to numbers
+    let num1: u128 = words.next().unwrap().parse().unwrap();
+    let num2: u128 = words.next().unwrap().parse().unwrap();
+
+    [num1, num2]
 }
